90d4d0a2e6bd7341a603babbcb2a01a52c281fa9
idnhadestech <idnhadestech@gmail.com>
2026-06-24 21:29:21 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,14 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/company", (req, res) => {
   res.json({
     name: "HADES TECH",
     founded: "Februari 2025",
-    founder: "HADI DEV",
-    location: "Temanggung, Jawa Tengah"
+    founder: "Suseno Hadi Maksum",
+    location: "Temanggung, Jawa Tengah",
+    focus: ["WhatsApp Bot", "PPOB System", "Software Development"]
   });
 });
 
