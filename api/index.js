98f772168e2390414ce5bdea6f90dc9edb189b6a
idnhadestech <idnhadestech@gmail.com>
2026-06-24 23:46:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,14 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+// API
 app.get("/api/company", (req, res) => {
-  res.json({
+  res.status(200).json({
     name: "HADES TECH",
     founded: "Februari 2025",
-    founder: "Suseno Hadi Maksum",
+    founder: "HADI DEV",
     location: "Temanggung, Jawa Tengah",
-    focus: ["WhatsApp Bot", "PPOB System", "Software Development"]
+    focus: [
+      "WhatsApp Bot",
+      "PPOB System",
+      "Software Development"
+    ]
   });
 });
 
-export default app;
+// Vercel handler
+export default function handler(req, res) {
+  return app(req, res);
+      }
